dedd9d1c7bcd1bb4509f81a137e378d6f4a671c1
ChenXiaoYingCaryn <739130811@qq.com>
2023-05-18 16:10:11 +0800

    update -- 移除启动类

--- src/main/java/com/example/corsplugindemo/CorsPluginDemoApplication.java
+++ src/main/java/com/example/corsplugindemo/CorsPluginDemoApplication.java
@@ -1,15 +1,16 @@
-package com.example.corsplugindemo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@SpringBootApplication
-@EnableConfigurationProperties
-public class CorsPluginDemoApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CorsPluginDemoApplication.class, args);
-    }
-
-}
+//package com.example.corsplugindemo;
+//
+//import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//
+//@SpringBootApplication
+//@EnableConfigurationProperties
+//public class CorsPluginDemoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CorsPluginDemoApplication.class, args);
+//    }
+//
+//}
